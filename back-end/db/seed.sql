f40a203d93732ac0cbdde0a1a541ae9a509a50d3
Taryne Leach Gonzalez <96316119+TaryneLeach@users.noreply.github.com>
2022-11-16 12:19:26 -0500

    added teacher values (#4)

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -2,3 +2,12 @@
 
 
 INSERT INTO books(book_title,book_author,isbn_number,publication,book_picture,reading_level) VALUES ('Night Owl',' Kim .C. Lee','978-0-578-71269-7','Kimberly Lee','https://images-us.bookshop.org/ingram/9780578712697.jpg?height=500&v=v2', 'C'),('Cookies Week','Cindy Ward','9780399243267','Putnam Juvenile','https://images.randomhouse.com/cover/9780698114357', 'F')
+
+
+
+
+INSERT INTO teachers(teacher_id,teacher_name,school_name,school_district,school_address,zipcode,state_name,class_subject) VALUES
+('Ms.Fundy', 'P.S. 152', '2', '725 East 23rd St',11210, 'NY'),
+('Ms Perez', 'P.S. 152', '2', '725 East 23rd St', 11210, 'NY'),
+('Mr.Edmundson','P.S. 315','4', '2310 Glenwood Rd', 11210, 'NY'),
+('Ms.Blackmond', 'P.S. 075', '13', '735 West End Ave', 10025, 'NY')
