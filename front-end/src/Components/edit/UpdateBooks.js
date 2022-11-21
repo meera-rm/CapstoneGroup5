2352369374cae429b63bfbed70c079d35f55317f
meera-rm <raamesh.meera@gmail.com>
2022-11-21 13:26:42 -0500

    editroutes

--- front-end/src/Components/edit/UpdateBooks.js
+++ front-end/src/Components/edit/UpdateBooks.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams, Link, useNavigate } from 'react-router-dom';
-// import Swal from 'sweetalert2';
 
 const API = process.env.REACT_APP_API_URL;
 
@@ -11,6 +10,7 @@ const UpdateBooks = () => {
   const navigate = useNavigate();
 
   const [book, setBook] = useState({
+    book_id: '',
     book_title: '',
     book_author: '',
     isbn_number: '',
@@ -26,18 +26,10 @@ const UpdateBooks = () => {
         setBook(res.data.payload);
       })
       .catch((e) => console.error(e));
-  }, [id]);
+    console.log('data=', book);
+  }, [id, book]);
 
-  const updatedBook = (book) => {
-    axios
-      .put(`${API}/api/books/${id}`, book)
-      .then((res) => {
-        navigate(`/books`);
-      })
-      // .error((error)=>console.error(error))
-      .catch((c) => console.warn('catch', c));
-  };
-  const handleTextChange = (event) => {
+  const onInputChange = (event) => {
     console.log(event.target.value);
     setBook({
       ...book,
@@ -47,114 +39,112 @@ const UpdateBooks = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    updatedBook(book, id);
+    axios
+      .put(`${API}/api/books/${id}`, book)
+      .then((res) => {
+        navigate(`/books`);
+      })
+      .catch((c) => console.warn('catch', c));
   };
 
   return (
-    <div className='edit-book'>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor='book_title'>Booktitle:</label>
-          <input
-            id='book_title'
-            name='book_title'
-            value={book.book_title}
-            type='text'
-            onChange={handleTextChange}
-            placeholder='Book title'
-          />
-        </div>
-        <div>
-          <label htmlFor='book_author'>Author:</label>
-          <input
-            id='book_author'
-            type='text'
-            name='book_author'
-            value={book.book_author}
-            placeholder='author'
-            onChange={handleTextChange}
-          />
-        </div>
-        <div>
-          <label htmlFor='isbn_number'>ISBN Number:</label>
-          <input
-            id='isbn_number'
-            type='text'
-            name='isbn_number'
-            value={book.isbn_number}
-            onChange={handleTextChange}
-          />
-        </div>
-        <div>
-          <label htmlFor='publication'>Publication:</label>
-          <input
-            id='publication'
-            name='publication'
-            type='text'
-            value={book.publication}
-            placeholder='publication'
-            onChange={handleTextChange}
-          />
-        </div>
-        <div>
-          <label htmlFor='book_picture'>Cover Image: </label>
-          <input
-            id='book_picture'
-            type='text'
-            name='book_picture'
-            value={book.book_picture}
-            placeholder='picture'
-            onChange={handleTextChange}
-          />
-        </div>
+    // <div className='edit-book'>
+    <div className='flex flex-col text-center h-screen items-center justify-center '>
+      <div className='rounded-md  py-4 px-6 text-black lg:w-2/5 md:w-3/5 w-4/5'>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label className='py-6' htmlFor='bookTitle'>
+              Booktitle:
+            </label>
+            <input
+              id='bookTitle'
+              name='bookTitle'
+              value={book.book_title}
+              type='text'
+              onChange={onInputChange}
+              placeholder='Book title'
+              required
+            />
+          </div>
+          <div>
+            <label className='py-6' htmlFor='author'>
+              Author:
+            </label>
+            <input
+              id='author'
+              type='text'
+              name='author'
+              value={book.book_author}
+              placeholder='author'
+              onChange={onInputChange}
+            />
+          </div>
+          <div>
+            <label className='py-6' htmlFor='isbn'>
+              ISBN Number:
+            </label>
+            <input
+              id='isnbe'
+              type='text'
+              name='isbn'
+              value={book.isbn_number}
+              onChange={onInputChange}
+            />
+          </div>
+          <div>
+            <label className='py-6' htmlFor='publication'>
+              Publication:
+            </label>
+            <input
+              id='publication'
+              name='publication'
+              type='text'
+              required
+              value={book.publication}
+              placeholder='publication'
+              onChange={onInputChange}
+            />
+          </div>
 
-        <div>
-          <label htmlFor='readinglevel'>Reading Level:</label>
-          <input
-            id='reading_level'
-            type='text'
-            name='reading_level'
-            value={book.reading_level}
-            onChange={handleTextChange}
-          />
-        </div>
+          <div>
+            <label className='py-6' htmlFor='picture'>
+              Cover Image:{' '}
+            </label>
+            <input
+              id='picture'
+              type='text'
+              name='picture'
+              value={book.book_picture}
+              placeholder='picture'
+              onChange={onInputChange}
+              required
+            />
+          </div>
+          <div>
+            <label className='py-6' htmlFor='readingLevel'>
+              Reading Level:
+            </label>
+            <input
+              id='readingLevel'
+              type='text'
+              name='readingLevel'
+              value={book.reading_level}
+              onChange={onInputChange}
+            />
+          </div>
 
-        <div
-          className='edit-btn'
-          style={{
-            display: 'flex',
-            justifyContent: 'space-around',
-            alignItems: 'center',
-            width: '100%',
-          }}
-        >
-          {/* <div> */}
-          <input
-            style={{
-              padding: '1rem 1.5rem',
-              borderRadius: '1rem',
-              background: 'transparent',
-            }}
-            className='update-btns second'
-            type='submit'
-          />
-          {/* </div>
+          <div className='justify-center ml-6 space-x-6 '>
+            {/* <div> */}
+            <input className=' px-5  py-3 rounded bg-teal-500' type='submit' />
+            {/* </div>
           <div> */}
-          <Link to={`/books/${id}`}>
-            <button
-              style={{
-                padding: '1rem 1.5rem',
-                borderRadius: '1rem',
-                background: 'transparent',
-              }}
-              className='update-btns'
-            >
-              Back
-            </button>
-          </Link>
-          {/* </div> */}
-        </div>
-      </form>
+            <Link to={`/books/${id}`}>
+              <button className=' px-5  py-3 rounded bg-teal-500'>Back</button>
+            </Link>
+            {/* </div> */}
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
