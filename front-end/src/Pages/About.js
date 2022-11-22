fd7c584428fd4edf58bf49e9de5d8a19622ac58d
meera-rm <raamesh.meera@gmail.com>
2022-11-22 10:50:10 -0500

    fixed errors

--- front-end/src/Pages/About.js
+++ front-end/src/Pages/About.js
@@ -1,4 +1,5 @@
 import { HelmetProvider, Helmet } from 'react-helmet-async';
+import aboutpic from '../Components/asset/studentstable.jpeg';
 const About = () => {
   return (
     <div>
@@ -9,38 +10,45 @@ const About = () => {
       </HelmetProvider>
       <h1 className='text-center text-teal-600 mt-6'>Scholar Sheep</h1>
 
-      <div className='home-header'>
-        <p
-          style={{
-            color: 'teal',
-            fontSize: 'x-large',
-            fontFamily: 'Georgia',
-            display: 'flex',
-          }}
-        >
-          <div>
-            <img
-              style={{
-                width: '650px',
-                display: 'flex',
-              }}
-              src='https://media1.thehungryjpeg.com/thumbs/800_3565494_tntw1101gpkivlxp8j525yx2u3vd6aqgdvm2aexb.jpg'
-              alt=''
-            />
-          </div>
-          <br></br>
-          <br></br>
-          <div>
-            Welcome to ScholarSheep, the one and only app for teachers and
+      {/* <div className='lg:flex justify-center mt-20 md:'>
+        <div className='relative h-0 mx-10 w-1/3 pb-2/3 sm:pt-1/3 lg:pt-1/3'>
+          <img className=' absolute inset-0 w-96 h-96'
+            src={aboutpic} alt=''/>
+        </div>
+
+        <div className='flex text-xl flex-col text-left items-center w-1/3'>
+          <p>
+          Welcome to ScholarSheep, the one and only app for teachers and
             parents to keep track of students' daily reading tasks. Our app
             incorporates a parent view and teacher view with the ability to log
             in for each view. Under the parent view, children can log their
             daily reading pages of the assigned reading. Under the teacher view,
             teachers can manage the reading list for their class, and keep track
             of each students progress.
-          </div>
-        </p>
-      </div>
+          </p>
+         </div>
+      </div> */}
+
+
+      <div class="mt-40 max-w-lg mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+  <div class="md:flex">
+    <div class="md:shrink-0">
+      <img class="h-48 w-full object-cover md:h-full md:w-80" src={aboutpic}alt="Modern building architecture"/>
+    </div>
+    <div class="p-8">
+     
+      <p class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Cultivating reading habits among children  </p>
+      <p class="mt-2 text-slate-500">Welcome to ScholarSheep, the one and only app for teachers and
+            parents to keep track of students' daily reading tasks. Our app
+            incorporates a parent view and teacher view with the ability to log
+            in for each view. Under the parent view, children can log their
+            daily reading pages of the assigned reading. Under the teacher view,
+            teachers can manage the reading list for their class, and keep track
+            of each students progress..</p>
+    </div>
+  </div>
+</div>
+      
     </div>
   );
 };
