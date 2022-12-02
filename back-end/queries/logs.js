e4e2d86b9f75dce3a6af80306088391fb38bee46
Derek <plutus.derekpeterson@gmail.com>
2022-12-02 14:25:24 -0500

    Fix student & log 1tomany

--- back-end/queries/logs.js
+++ back-end/queries/logs.js
@@ -1,8 +1,11 @@
 const db = require('../db/dbConfig.js');
 
-const getAllLogs = async () => {
+const getAllLogs = async (studentId) => {
   try {
-    const allLogs = await db.any('SELECT * FROM logs');
+    const allLogs = await db.any(
+      'SELECT * FROM logs WHERE student_id = $1',
+      studentId
+    );
     return allLogs;
   } catch (error) {
     return error;
@@ -69,8 +72,6 @@ const updateLog = async (id, student) => {
     return error;
   }
 };
-
-
 
 module.exports = {
   getAllLogs,
