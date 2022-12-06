3349a59ac6cd42fa112dd0bb2b9af22b2d744709
TaryneLeach <taryneleach@gmail.com>
2022-12-06 01:03:05 -0500

    fire-base-auth

--- front-end/src/Pages/Login.js
+++ front-end/src/Pages/Login.js
@@ -1,125 +1,119 @@
-import { useNavigate, useParams } from 'react-router-dom';
+import React from 'react';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { useState } from 'react';
-import axios from 'axios';
-import parentandchild from '../Components/asset/parentandchild.jpeg';
+import { useNavigate, useParams } from 'react-router-dom';
 import logoImage from '../Components/asset/ScholarSheep.png';
-
-const API = process.env.REACT_APP_API_URL;
+import app from '../firebase';
 
 const Login = () => {
-  const navigate = useNavigate();
-  const id = useParams();
-  const [user, setUser] = useState({
-    
-    user_email: '',
-    user_password: ''
-  });
+	const navigate = useNavigate();
+	const id = useParams();
+	const [user, setUser] = useState({
+		user_email: '',
+		user_password: '',
+	});
 
-  const userLogin = (event) => {
-    setUser({ ...user, [event.target.id]: event.target.value });
-  };
+	const auth = getAuth(app);
 
-  //  not sure if I need axios for this not sure how to add functionality
-  const handleLogIn = (user) => {
-    console.log('Login successful');
-    // axios
-    //     .post(`${API}/parent-login`, students)
-    //     .then(() => {
-    //         navigate(`/parent-page`);
-    //     })
-    //     .catch((error) => {
-    //         console.warn(error);
-    //     });
-  };
+	const handleChange = (event) => {
+		setUser({ ...user, [event.target.id]: event.target.value });
+	};
 
-  const handleForm = (event) => {
-    event.preventDefault();
-    handleLogIn(user, id);
-  };
+	const login = () => {
+		signInWithEmailAndPassword(auth, user.user_email, user.user_password)
+			.then((userCredential) => {
+				const user = userCredential.user;
+				if (user) {
+					alert(`Welcome to your login ${user.user_email}!`);
+					navigate('/home');
+				}
+				// ...
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				console.log(`${user.user_email} is not registered`);
+				const errorMessage = error.message;
+			});
+	};
 
-  return (
-    <div>
-      {/* <h1 class='text-center'>Parent Login</h1> */}
-      {/* <div class='flex items-center justify-center'>
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(user);
+		login();
+	};
+
+	return (
+		<div>
+			{/* <h1 class='text-center'>Parent Login</h1> */}
+			{/* <div class='flex items-center justify-center'>
       <img class=' h-22 w-48 rounded-full' src={parentandchild} alt='' />
       </div> */}
-      <div class='flex  items-center h-screen w-full bg-teal-lighter'>
-        <div class='w-full bg-white rounded shadow-lg p-8 m-2 md:max-w-sm md:mx-auto'>
-          <div class='flex items-center justify-center'>
-            <img class=' h-12 w-18 rounded-full' src={logoImage} alt='' />
-          </div>
+			<div class='flex  items-center h-screen w-full bg-teal-lighter'>
+				<div class='w-full bg-white rounded shadow-lg p-8 m-2 md:max-w-sm md:mx-auto'>
+					<div class='flex items-center justify-center'>
+						<img class=' h-12 w-18 rounded-full' src={logoImage} alt='' />
+					</div>
 
-          <h1 class='block w-full text-center text-grey-darkest mb-6'>
-            {' '}
-            Login{' '}
-          </h1>
-          <form
-            class='mb-4 md:flex md:flex-wrap md:justify-between'
-            onSubmit={handleForm}
-          >
-           
-            <div class='flex flex-col mb-4 md:w-full'>
-              <label
-                class='mb-2 uppercase font-bold text-lg text-grey-darkest'
-                htmlFor='user_email'
-              >
-                Email
-              </label>
-              <input
-                class='border py-2 px-3 text-grey-darkest'
-                type='email'
-                name='user_email'
-                id='user_email'
-                defaultValue={user.user_email}
-                onChange={userLogin}
-              />
-            </div>
+					<h1 class='block w-full text-center text-grey-darkest mb-6'>
+						{' '}
+						Login{' '}
+					</h1>
+					<form
+						class='mb-4 md:flex md:flex-wrap md:justify-between'
+						onSubmit={handleSubmit}
+					>
+						<div class='flex flex-col mb-4 md:w-full'>
+							<label
+								class='mb-2 uppercase font-bold text-lg text-grey-darkest'
+								htmlFor='user_email'
+							>
+								Email
+							</label>
+							<input
+								class='border py-2 px-3 text-grey-darkest'
+								type='email'
+								name='user_email'
+								id='user_email'
+								defaultValue={user.user_email}
+								onChange={handleChange}
+							/>
+						</div>
 
-            <div class='flex flex-col mb-6 md:w-full'>
-              <label
-                class='mb-2 uppercase font-bold text-lg text-grey-darkest'
-                htmlFor='user_password'
-              >
-                Password
-              </label>
-              <input
-                class='border py-2 px-3 text-grey-darkest'
-                type='password'
-                name='user_password'
-                id='user_password'
-                defaultValue={user.user_password}
-                onChange={userLogin}
-              />
-            </div>
+						<div class='flex flex-col mb-6 md:w-full'>
+							<label
+								class='mb-2 uppercase font-bold text-lg text-grey-darkest'
+								htmlFor='user_password'
+							>
+								Password
+							</label>
+							<input
+								class='border py-2 px-3 text-grey-darkest'
+								type='password'
+								name='user_password'
+								id='user_password'
+								defaultValue={user.user_password}
+								onChange={handleChange}
+							/>
+						</div>
 
-            <button
-              class='block bg-teal-600 hover:bg-teal-dark text-white uppercase text-lg mx-auto p-4 rounded'
-              type='submit'
-            >
-              Login
-            </button>
-          </form>
+						<button
+							class='block bg-teal-600 hover:bg-teal-dark text-white uppercase text-lg mx-auto p-4 rounded'
+							type='submit'
+						>
+							Login
+						</button>
+					</form>
 
-          <a
-            class='block w-full text-center no-underline text-sm text-grey-dark hover:text-grey-darker'
-            href='/signup'
-          >
-            Not Registered yet?<span class='font-bold'>Sign Up</span>
-          </a>
-          
-
-        </div>
-      </div>
-    </div>
-  );
+					<a
+						class='block w-full text-center no-underline text-sm text-grey-dark hover:text-grey-darker'
+						href='/signup'
+					>
+						Not Registered yet?<span class='font-bold'>Sign Up</span>
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
-
-
-
-
-
-
-
-   
