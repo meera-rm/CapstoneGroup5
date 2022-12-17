556c6fa29e029f43760fa84c7a4727b5d230292e
meera-rm <raamesh.meera@gmail.com>
2022-12-17 13:49:02 -0500

    studentandbooks

--- front-end/src/Components/show/StudentDetails.js
+++ front-end/src/Components/show/StudentDetails.js
@@ -258,17 +258,31 @@ const StudentDetails = () => {
                         {/* // ) : (
                           // </td>
                           // <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
+                          form openai
+                           const [showMessage, setShowMessage] = useState(false);
+
+
+    <div>
+      {showMessage ? (
+        <p>This is a message.</p>
+      ) : (
+        <button onClick={() => setShowMessage(true)}>
+          Show message
+        </button>
+      )}
+    </div>
+    endof oepnai
                           //  <div className='ml-3 p-3 text-sm text-indigo-900'>
                           // <Link to={`/comments/new`}>
                           //   <button */}
                           {/* //     className=' bg-teal-500 px-6 py-4 text-black rounded '
-                          //     onClick={onClick}
+                          //     onClick={() => setMessage(true)}
                           //   >
                           //     <MdAddComment />{' '}
                           //   </button>
                           // </Link> */}
                         {/* // 
-                         </div> */} */}
+                         </div> */} 
                       </td>
 
                       {/* <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
