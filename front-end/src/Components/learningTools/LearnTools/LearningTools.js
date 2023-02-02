5dd40c459be1f2490ecc34426b6fd4334a1bf83c
meera-rm <raamesh.meera@gmail.com>
2023-02-02 01:17:44 -0500

    learningtools

---
+++ front-end/src/Components/learningTools/LearnTools/LearningTools.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+
+
+const LearningTools =() =>{
+
+  return (
+    <div class='learningTools'>
+        <div class='learningTools__title'>
+         Learning Tools for Enhanced Vocabulary
+          </div>
+
+       <div class='learningTools__container'>
+
+          <div class='learningTools__item'>
+             <img src='https://i.imgur.com/L2NmYX9.png' alt='' />
+              <div class='learningTools__text'>
+                 <h2>Dictionary Lookup</h2>
+              <h4>Find the definition of a word/pronounciation</h4>
+              </div>
+           </div> 
+
+
+
+         <div class='learningTools__item'>
+             <img src='https://i.imgur.com/OvDjHRw.png' alt='' />
+             <div class='learningTools__item-text'>
+                  <h2>Vocabulary Words</h2>
+                  <h4>Practice Vocabulary words </h4>
+             </div>
+        </div> 
+     </div>
+    </div>
+  )
+
+}
+
+export default LearningTools;
