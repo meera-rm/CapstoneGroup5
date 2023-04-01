7045139f7789bb0aba0353d3920ed28facad0f23
meera-rm <raamesh.meera@gmail.com>
2023-03-31 22:56:55 -0400

    deleted front-end folder

--- front-end/src/utils/AvatarProfilePics.js
+++
@@ -1,9 +0,0 @@
-const avatarProfilePics = (avatarNames)=>{
-
-    const randomNumber = ( Math.floor(Math.random() * (avatarNames.length)));
-    const avatarProfile=`https://api.dicebear.com/5.x/pixel-art/svg?seed=${avatarNames[randomNumber]}`;
-  
-    return avatarProfile;
-}
-
-export default avatarProfilePics;
